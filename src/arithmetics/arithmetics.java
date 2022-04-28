cc2e6284155fddae689be2e6385cfb00823b4497
Kekyurin <kekyurinday@gmail.com>
2022-04-28 14:28:19 +0300

    first commit

---
+++ src/arithmetics/arithmetics.java
@@ -0,0 +1,22 @@
+package arithmetics;
+
+public class arithmetics {
+
+	public static void main(String[] args) {
+		int a, b;
+		a = 11;
+		b = 5;
+		int c = a / b;
+		int d = a % b;
+		System.out.println("Ділення - " +c);
+		System.out.println("Залишок від ділення - " +d);
+		
+		int x;
+		x = 60 + 33;
+		System.out.println(x);
+		
+		double y = 2.4;
+		System.out.println(Math.round(y));
+	}
+
+}
